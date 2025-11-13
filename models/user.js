bec678189b0d86af4ecb985ff91be540a36915f0
AyushZorix <ayushbhandaristudy@gmail.com>
2025-11-13 22:01:57 +0530

    added some changes

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-const { createHmac, randomBytes } = require("crypto");
+const { randomBytes, scryptSync, timingSafeEqual } = require("crypto");
 const { Schema, model } = require("mongoose");
 const { createTokenForUser } = require("../service/authentication");
 
@@ -27,18 +27,23 @@ const userSchema = new Schema({
     type: String,
     enum: ["USER", "ADMIN"],
     default: "USER",
-  }
+  },
+  lastLoginAt: {
+    type: Date,
+  },
 }, { timestamps: true });
 
 userSchema.pre("save", function (next) {
   const user = this;
 
+  if (user.isModified("email") && typeof user.email === "string") {
+    user.email = user.email.toLowerCase();
+  }
+
   if (!user.isModified("password")) return next();
 
   const salt = randomBytes(16).toString("hex");
-  const hashedPassword = createHmac("sha256", salt)
-    .update(user.password)
-    .digest("hex");
+  const hashedPassword = scryptSync(user.password, salt, 64).toString("hex");
 
   user.salt = salt;
   user.password = hashedPassword;
@@ -49,14 +54,21 @@ userSchema.pre("save", function (next) {
 userSchema.static(
   "matchPasswordAndGenerateToken",
   async function (email, password) {
-    const user = await this.findOne({ email });
+    const normalizedEmail = typeof email === "string" ? email.toLowerCase() : email;
+    const user = await this.findOne({ email: normalizedEmail });
     if (!user) throw new Error("User not found!");
 
-    const userProvidedHash = createHmac("sha256", user.salt)
-      .update(password)
-      .digest("hex");
+    if (!user.salt || !user.password) {
+      throw new Error("Credentials not set up correctly");
+    }
 
-    if (user.password !== userProvidedHash) throw new Error("Incorrect Password");
+    const storedHash = Buffer.from(user.password, "hex");
+    const userProvidedHash = scryptSync(password, user.salt, storedHash.length);
+
+    if (!timingSafeEqual(storedHash, userProvidedHash)) throw new Error("Incorrect Password");
+
+    user.lastLoginAt = new Date();
+    await user.save({ validateBeforeSave: false });
 
     const token = createTokenForUser(user);
     return token;
